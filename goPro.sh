8e7d4d73a689c82dd79008a374e97739134d7cd9
Tianqin Li <jacklitianqin@gmail.com>
2019-08-24 04:40:50 +0000

    basic set

---
+++ goPro.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+
+# vim
+echo "Costimize VIM now..."
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+echo "set nu!" >> ~/.vimrc
+echo "set rnu" >> ~/.vimrc
+
+# oh-my-zsh
+echo "Costimize to zsh now..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
