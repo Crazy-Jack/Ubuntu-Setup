d3453fd04f2768ce4b9f6faf76022b11a0858445
Tianqin Li <jacklitianqin@gmail.com>
2019-08-24 05:13:25 +0000

    before push

--- goPro.sh
+++ goPro.sh
@@ -1,11 +1,12 @@
 #! /usr/bin/bash
 
+
 # vim
 echo "Costimize VIM now..."
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-echo "set nu!" >> ~/.vimrc
+echo "\nset nu!" >> ~/.vimrc
 echo "set rnu" >> ~/.vimrc
 
 # oh-my-zsh
