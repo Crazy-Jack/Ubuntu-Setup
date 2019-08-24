ea173bc804a23d0eec80e29a46d480fd9608d46d
Tianqin Li <jacklitianqin@gmail.com>
2019-08-24 05:34:58 +0000

    upgrade for install zsh and add github user

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 This repo contains a bash script for installing my costomized configuration for any new VPS. Generally, it contains the following installations:
 
 - [VIM](https://github.com/amix/vimrc) with ultimate configuration by Amix.
+- [ZSH](https://github.com/robbyrussell/oh-my-zsh/wiki/Installing-ZSH) Z Shell.
 - [Oh my zsh](https://github.com/robbyrussell/oh-my-zsh) oh-my-zsh community.
 
 
--- goPro.sh
+++ goPro.sh
@@ -2,15 +2,36 @@
 
 
 # vim
-echo "Costimize VIM now..."
+echo ""
+echo "*** BEGIN COSTOMIZING ***"
+echo "## Costimize VIM now..."
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-echo "\nset nu!" >> ~/.vimrc
+echo "set nu!" >> ~/.vimrc
 echo "set rnu" >> ~/.vimrc
+echo "Done!"
+echo "-----------------"
+
+# zsh
+echo "## Installing zsh first.."
+sudo apt install zsh
+echo "Done!"
+echo "-----------------"
 
 # oh-my-zsh
-echo "Costimize to zsh now..."
+echo "## Costimize to zsh now..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "Done!"
+echo "-----------------"
 
 
+# add user
+echo "## Add user for github..."
+git config --global user.email "jacklitianqin@gmail.com"
+git config --global user.name "Tianqin Li"
+echo "Done!"
+echo "-----------------"
+
+echo "ALL DONE, you are a pro now. Enjoy! :)"
+echo ""
