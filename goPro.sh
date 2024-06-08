67b08bdc4ef021e9efc48cc584aabe089f04d7d8
Tianqin Li <jacklitianqin@gmail.com>
2024-06-08 03:54:55 -0400

    Update goPro.sh

--- goPro.sh
+++ goPro.sh
@@ -21,15 +21,6 @@ echo "Done!"
 echo "-----------------"
 
 
-# configue backblaze
-cd ~
-wget https://github.com/Backblaze/B2_Command_Line_Tool/releases/latest/download/b2-linux
-mv b2-linux b2
-chmod +x b2 
-
-./b2 authorize_account
-
-
 # oh-my-zsh
 echo "## Costimize to zsh now..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
